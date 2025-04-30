5a002f62bbb18db9d7ecb31e7e29f85d99da0fc8
jashnnkhuranaa <jashnkhurana@gmail.com>
2025-04-30 18:18:16 +0530

    data table created

---
+++ finance-platform/app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Payment, columns } from "./columns"
+import { DataTable } from "@/components/accounts/data-table"
+
+const data: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@gmail.com",
+  },
+]
+
+const AccountsPage = () => {
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none">
+  <CardHeader className="border-none px-6">
+    <div className="border-none flex w-full items-center justify-between">
+      <CardTitle className="text-xl line-clamp-1">Accounts</CardTitle>
+      <Button size="sm">
+        <Plus className="size-4 mr-2" />
+        Add Account
+      </Button>
+    </div>
+  </CardHeader>
+
+  <CardContent>
+    <DataTable columns={columns} data={data} />
+  </CardContent>
+</Card>
+
+    </div>
+  );
+};
+
+export default AccountsPage;
