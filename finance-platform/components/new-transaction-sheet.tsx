2a2edf7be0e5d23bc28d1aca9b32e925e438b979
jashnnkhuranaa <jashnkhurana@gmail.com>
2025-05-02 14:17:02 +0530

    add transaction feature added

--- finance-platform/components/new-transaction-sheet.tsx
+++ finance-platform/components/new-transaction-sheet.tsx
@@ -1,22 +1,22 @@
 
-// // components/new-transaction-sheet.tsx
-// 'use client';
+// components/new-transaction-sheet.tsx
+'use client';
 
-// import { useNewAccount } from '@/app/api/accounts/hooks/use-new-account';
-// import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
-// import { TransactionForm } from './transaction-form';
+import { useNewAccount } from '@/app/api/accounts/hooks/use-new-account';
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
+import { TransactionForm } from './transaction-form';
 
-// export function NewTransactionSheet() {
-//   const { isOpen, onClose } = useNewAccount();
+export function NewTransactionSheet() {
+  const { isOpen, onClose } = useNewAccount();
 
-//   return (
-//     <Sheet open={isOpen} onOpenChange={onClose}>
-//       <SheetContent className="space-y-4">
-//         <SheetHeader>
-//           <SheetTitle>Add a new transaction</SheetTitle>
-//         </SheetHeader>
-//         <TransactionForm onSuccess={onClose} />
-//       </SheetContent>
-//     </Sheet>
-//   );
-// }
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>Add a new transaction</SheetTitle>
+        </SheetHeader>
+        <TransactionForm onSuccess={onClose} />
+      </SheetContent>
+    </Sheet>
+  );
+}
